0c1315ff9409b44b3b1d2ff15cabf75485252a21
fidel <fidelbello1997@gmail.com>
2023-04-12 23:25:02 -0400

    added win32 api functions

---
+++ debugging/Imports/Win32.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace debugging.Imports
+{
+        public class Win32
+        {
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, bool bInheritHandle, Int32 dwProcessId);
+
+        [DllImport("kernel32")]
+        public static extern bool IsWow64Process(IntPtr hProcess, out bool lpSystemInfo);
+    }
+}
