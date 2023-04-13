01129233162887ca6d074f047caf3b27135e6b1b
fidel <fidelbello1997@gmail.com>
2023-04-12 23:33:05 -0400

    reformat

--- debugging/Program.cs
+++ debugging/Program.cs
@@ -3,21 +3,24 @@ internal class Program
 {
     private static int Main(string[] args)
     {
-       Debug debug = new();
-       int Found =  debug.FindProccessByName("discord");
+        Debug debug = new();
+        int Found = debug.FindProccessByName("discord");
         bool DidFindModule = debug.OpenProcess(Found);
-       if(Found != -1)
-        {
 
+        if (Found != -1)
+        {
             Console.WriteLine($"Process Id is: {Found}");
-            if( DidFindModule != false)
+            if (DidFindModule != false)
             {
                 Console.WriteLine("Working and getting main module");
-            } else
+            }
+            else
             {
                 Console.WriteLine("Still needs Some work");
             }
-        } else
+
+        }
+        else
         {
             Console.Error.WriteLine("Not Found");
         }
