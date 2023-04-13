c8db9eb0a8ba47b61b3bed04a1cef0b1b28c5902
fidel <fidelbello1997@gmail.com>
2023-04-12 23:27:01 -0400

    main module for discord was found

--- debugging/Program.cs
+++ debugging/Program.cs
@@ -1,16 +1,27 @@
-﻿using debugging.mem;
+﻿using debugging.DebugClass;
 internal class Program
 {
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
        Debug debug = new();
        int Found =  debug.FindProccessByName("discord");
+        bool DidFindModule = debug.OpenProcess(Found);
        if(Found != -1)
         {
+
             Console.WriteLine($"Process Id is: {Found}");
+            if( DidFindModule != false)
+            {
+                Console.WriteLine("Working and getting main module");
+            } else
+            {
+                Console.WriteLine("Still needs Some work");
+            }
         } else
         {
             Console.Error.WriteLine("Not Found");
         }
+
+        return 0;
     }
 }
