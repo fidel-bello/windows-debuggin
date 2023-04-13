89fcc0beb1323798383d7f265f2b5863de339ff8
fidel <fidelbello1997@gmail.com>
2023-04-12 21:52:20 -0400

    added debug class that extends process class from windows api

---
+++ debugging/DebugClass/Debugging.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace debugging.mem
+{
+    internal class Debug : Process
+    {
+        public int FindProccessByName(string name)
+        {
+            Process[] Procs = GetProcesses();
+            foreach (Process proc in Procs)
+            {
+                if(proc.ProcessName.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return proc.Id;
+                }
+            }
+            return -1;
+        }
+    }
+}
