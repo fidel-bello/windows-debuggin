fa5abc7322023b15a18b18fca847df7da96d44cd
fidel <fidelbello1997@gmail.com>
2023-04-12 21:52:50 -0400

    created instance of debug and print out discord proc id

--- debugging/Program.cs
+++ debugging/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using debugging.mem;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       Debug debug = new();
+       int Found =  debug.FindProccessByName("discord");
+       if(Found != -1)
+        {
+            Console.WriteLine($"Process Id is: {Found}");
+        } else
+        {
+            Console.Error.WriteLine("Not Found");
+        }
     }
 }
