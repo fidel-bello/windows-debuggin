7d21b2d09e63732e2ad8836cf4e72329a190b1a0
fidel <fidelbello1997@gmail.com>
2023-04-12 23:28:59 -0400

    added prettier

--- debugging/Imports/Win32.cs
+++ debugging/Imports/Win32.cs
@@ -9,8 +9,8 @@ namespace debugging.Imports
 {
         public class Win32
         {
-            [DllImport("kernel32.dll")]
-            public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, bool bInheritHandle, Int32 dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, bool bInheritHandle, Int32 dwProcessId);
 
         [DllImport("kernel32")]
         public static extern bool IsWow64Process(IntPtr hProcess, out bool lpSystemInfo);
